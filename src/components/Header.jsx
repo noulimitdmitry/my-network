f26f4404da0a007fefd1df313fba455220eedaf8
noulimitdmitry <kursant7321@gmail.com>
2023-01-26 11:22:36 +0200

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@
 const Header = () => {
   return (
     <header className="app__header header">
+      Header
       <img className="header__img logo" src="https://st.depositphotos.com/1010751/5000/v/600/depositphotos_50006453-stock-illustration-teamwork-hi-5-lineal-image.jpg"></img>
     </header>
   );
